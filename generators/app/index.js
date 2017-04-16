b729d19f5bb13f1bac2408d20f11ebe4b05e784f
Tarun M <tmukherjee13@gmail.com>
2017-04-17 00:28:15 +0530

    modified theme files

--- generators/app/index.js
+++ generators/app/index.js
@@ -4,56 +4,55 @@ const chalk = require('chalk');
 const yosay = require('yosay');
 const _ = require('lodash-addons');
 module.exports = class extends Generator {
-  constructor(arg, opts) {
-    super(arg, opts);
-    this.option('front');
-    this.appPool = (this.options.front ? 'frontend' : 'backend');
-  }
-  prompting() {
-    this.log(yosay('Welcome to the astonishing ' + chalk.red('generator-ng-ui') + ' generator!'));
-    const prompts = [{
-      type: 'input',
-      name: 'name',
-      message: 'Your project name',
-      default: this.appname
-    }, {
-      type: 'input',
-      name: 'url',
-      message: 'Your project\'s url',
-      default: 'http://localhost'
-    }];
-    return this.prompt(prompts)
-      .then(props => {
-        // To access props later use this.props.someAnswer;
-        this.props = props;
-      });
-  }
-  writing() {
-    this.fs.copy(this.templatePath('dummyfile.txt'), this.destinationPath('dummyfile.txt'));
-    // Copy the configuration files
-    let config = function () {
-      this.fs.copyTpl(this.templatePath('config/_package.json'), this.destinationPath('package.json'), {
-        _: _,
-        name: this.props.name,
-        props: this.props
-      });
-      // If webpack is enabled
-      // this.fs.copyTpl(this.templatePath('config/_webpack.config.js'), this.destinationPath('webpack.config.js'), {
-      //   _: _
-      // });
-    };
-    // Copy application files
-    let app = function () {
-      this.composeWith('ng-ui:theme', {
-        theme: 'default',
-        name: this.props.name
-      });
-    };
-    config.call(this);
-    app.call(this);
-  }
-  install() {
-    // Install Dependencies
-    // this.installDependencies();
-  }
+    constructor(arg, opts) {
+        super(arg, opts);
+        this.option('front');
+        this.appPool = (this.options.front ? 'frontend' : 'backend');
+    }
+    prompting() {
+        this.log(yosay('Welcome to the astonishing ' + chalk.red('generator-ng-ui') + ' generator!'));
+        const prompts = [{
+            type: 'input',
+            name: 'name',
+            message: 'Your project name',
+            default: this.appname
+        }, {
+            type: 'input',
+            name: 'url',
+            message: 'Your project\'s url',
+            default: 'http://localhost'
+        }];
+        return this.prompt(prompts).then(props => {
+            // To access props later use this.props.someAnswer;
+            this.props = props;
+        });
+    }
+    writing() {
+
+        // Copy the configuration files
+        let config = function() {
+            this.fs.copyTpl(this.templatePath('config/_package.json'), this.destinationPath('package.json'), {
+                _: _,
+                name: this.props.name,
+                props: this.props
+            });
+            // If webpack is enabled
+            // this.fs.copyTpl(this.templatePath('config/_webpack.config.js'), this.destinationPath('webpack.config.js'), {
+            //   _: _
+            // });
+        };
+        // Copy application files
+        let app = function() {
+            this.composeWith('ng-ui:theme', {
+                theme: 'default',
+                name: this.props.name
+            });
+        };
+        config.call(this);
+        app.call(this);
+    }
+    install() {
+        // Install Dependencies
+        // this.installDependencies();
+    }
 };
--- generators/theme/index.js
+++ generators/theme/index.js
@@ -1,80 +1,95 @@
 'use strict';
 const Generator = require('yeoman-generator');
 const _ = require('lodash-addons');
+const path = require('path');
+const walk = require(path.join(__dirname, '..', '..', 'lib', 'functions')).walk;
 module.exports = class extends Generator {
-  prompting() {
-    const prompts = [{
-      type: 'input',
-      name: 'theme',
-      message: 'Which theme would you  like to use?',
-      default: 'default'
-    }, {
-      type: 'checkbox',
-      name: 'modules',
-      message: 'Which modules would you like to install?',
-      default: true,
-      choices: [{
-        name: 'Auth',
-        value: 'auth',
-        checked: true
-      }, {
-        name: 'User',
-        value: 'user',
-        checked: false
-      }]
-    }, {
-      type: 'confirm',
-      name: 'setup',
-      message: 'Would you like proceed with the setup?',
-      default: true
-    }];
-    return this.prompt(prompts)
-      .then(props => {
-        // To access props later use this.props.someAnswer;
-        this.props = props;
-      });
-  }
-  writing() {
-    console.log(this.props.modules);
-    let enabledModules = {};
-    this.props.modules.map(m => enabledModules[m] = true );
-
-    let config = function () {
-      this.fs.copy(this.templatePath('config/bowerrc'), this.destinationPath('.bowerrc'));
-      this.fs.copyTpl(this.templatePath('config/_bower.json'), this.destinationPath('bower.json'), {
-        _: _,
-        name: this.options.name
-      });
-
-      this.fs.copy(this.templatePath('files'), this.destinationPath('public'), {
-        _: _,
-        name: this.options.name
-      });
-      this.fs.copyTpl(this.templatePath('files/js/main.js'), this.destinationPath('public/js/main.js'), {
-        _: _,
-        name: this.options.name
-      });
-
-      this.fs.copyTpl(this.templatePath('files/js/config.js'), this.destinationPath('public/js/config.js'), {
-        _: _,
-        enabledModules: enabledModules
-      });
-
-      this.fs.copyTpl(this.templatePath('files/js/router.js'), this.destinationPath('public/js/router.js'), {
-        _: _,
-        enabledModules: enabledModules
-      });
-
-      this.fs.copyTpl(this.templatePath('files/index.html'), this.destinationPath('public/index.html'), {
-        _: _,
-        name: this.options.name,
-        enabledModules: enabledModules
-      });
-    };
-    config.call(this);
-  }
-  install() {
-    // Install Dependencies
-    // this.installDependencies();
-  }
+    prompting() {
+        const prompts = [{
+            type: 'input',
+            name: 'theme',
+            message: 'Which theme would you  like to use?',
+            default: 'default'
+        }, {
+            type: 'checkbox',
+            name: 'modules',
+            message: 'Which modules would you like to install?',
+            default: true,
+            choices: [{
+                name: 'Auth',
+                value: 'auth',
+                checked: true
+            }, {
+                name: 'User',
+                value: 'user',
+                checked: false
+            }]
+        }, {
+            type: 'confirm',
+            name: 'setup',
+            message: 'Would you like proceed with the setup?',
+            default: true
+        }];
+        return this.prompt(prompts).then(props => {
+            // To access props later use this.props.someAnswer;
+            this.props = props;
+        });
+    }
+    writing() {
+        console.log(this.props.modules);
+        let enabledModules = {};
+        this.props.modules.forEach(m => enabledModules[m] = true);
+        let config = function() {
+            this.fs.copy(this.templatePath('config/bowerrc'), this.destinationPath('.bowerrc'));
+            this.fs.copyTpl(this.templatePath('config/_bower.json'), this.destinationPath('bower.json'), {
+                _: _,
+                name: this.options.name
+            });
+            this.fs.copy(this.templatePath('files'), this.destinationPath('public'), {
+                _: _,
+                name: this.options.name
+            });
+            this.fs.copyTpl(this.templatePath('files/js/main.js'), this.destinationPath('public/js/main.js'), {
+                _: _,
+                name: this.options.name
+            });
+            this.fs.copyTpl(this.templatePath('files/js/config.js'), this.destinationPath('public/js/config.js'), {
+                _: _,
+                enabledModules: enabledModules
+            });
+            this.fs.copyTpl(this.templatePath('files/js/router.js'), this.destinationPath('public/js/router.js'), {
+                _: _,
+                enabledModules: enabledModules
+            });
+            this.fs.copyTpl(this.templatePath('files/index.html'), this.destinationPath('public/index.html'), {
+                _: _,
+                name: this.options.name,
+                enabledModules: enabledModules
+            });
+        };
+        let files = function() {
+            var vm = this;
+            walk(this.templatePath('files'), function(err, results) {
+                if (err) throw err;
+                results.forEach(f => {
+                    let fileName = f.split('files' + path.sep)[1];
+                    if (f.split(path.sep).pop().startsWith('_')) {
+                        vm.fs.copyTpl(vm.templatePath(path.join('files', fileName)), vm.destinationPath(path.join('public', fileName.replace('_',''))), {
+                            _: _,
+                            name: vm.options.name,
+                            enabledModules: enabledModules
+                        });
+                    } else {
+                        vm.fs.copy(vm.templatePath(path.join('files', fileName)), vm.destinationPath(path.join('public', fileName)));
+                    }
+                });
+            });
+        };
+        // config.call(this);
+        files.call(this);
+    }
+    install() {
+        // Install Dependencies
+        // this.installDependencies();
+    }
 };
